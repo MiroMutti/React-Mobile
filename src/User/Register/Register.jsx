dd6100f4115755039218bd511e022e78f0a68529
MiroMutti <cvetomir_vt@icloud.com>
2019-12-10 23:59:29 +0200

    Forms and validations

--- src/User/Register/Register.jsx
+++ src/User/Register/Register.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import * as yup from 'yup';
+
 import { NavLink } from 'react-router-dom';
 import withForm from '../../hocs/formManager';
 
 
-class Register extends React.Component(){
+class Register extends React.Component {
 
-    render(){
+    usernameOnChangeHandler = this.props.controlChangeHandlerFactory('username');
+    emailOnChangeHandler = this.props.controlChangeHandlerFactory('email');
+    passwordOnChangeHandler = this.props.controlChangeHandlerFactory('password');
+    rePasswordOnChangeHandler = this.props.controlChangeHandlerFactory('rePassword');
+
+    submitHandler = () => {
+        this.props.runValidations()
+            .then(formData => console.log(formData))
+    }
+
+    getFirstControlError = name => {
+        const errorState = this.props.getFormErrorState();
+        return errorState && errorState[name] && errorState[name][0];
+    }
+
+    render() {
+        const usernameError = this.getFirstControlError('username');
+        const emailError = this.getFirstControlError('email');
+        const passwordError = this.getFirstControlError('password');
+        const rePasswordError = this.getFirstControlError('rePassword');
+
         return (
             <div className="form-wrapper">
                 <h2>Login</h2>
@@ -16,9 +38,10 @@ class Register extends React.Component(){
                             type="text"
                             name="username"
                             placeholder="enter username"
-                            minlength='5'
-                            
-                            />
+                            onChange={this.usernameOnChangeHandler}
+
+                        />
+                        {usernameError && <div className="error">{usernameError}</div>}
                     </label>
                     <label htmlFor="email">
                         E-mail
@@ -26,7 +49,10 @@ class Register extends React.Component(){
                             type="email"
                             name="email"
                             placeholder="enter your email"
-                            />
+                            onChange={this.emailOnChangeHandler}
+
+                        />
+                        {emailError && <div className="error">{emailError}</div>}
                     </label>
                     <label htmlFor="password">
                         Password
@@ -34,10 +60,23 @@ class Register extends React.Component(){
                             type="password"
                             name="password"
                             placeholder="enter password"
-                            minlength='6'
-                            />
+                            onChange={this.passwordOnChangeHandler}
+
+                        />
+                        {passwordError && <div className="error">{passwordError}</div>}
                     </label>
-                    <button className="button" type="submit">Register</button>
+                    <label htmlFor="rePassword">
+                        Repeat Password
+                        <input
+                            type="password"
+                            name="rePassword"
+                            placeholder="Repeat your password"
+                            onChange={this.rePasswordOnChangeHandler}
+
+                        />
+                        {rePasswordError && <div className="error">{rePasswordError}</div>}
+                    </label>
+                    <button className="button" type="button" onClick={this.submitHandler}>Register</button>
                 </form>
                 <div className="paragraph">You already have an account? <NavLink to="/login" className="button">Login</NavLink></div>
             </div>
@@ -45,4 +84,30 @@ class Register extends React.Component(){
     }
 }
 
-export default withForm(Register)
+const initialFormState = {
+    username: '',
+    password: '',
+    rePassword: ''
+}
+
+const schema = yup.object({
+    username: yup.string('Username should be string')
+        .required('Username is required')
+        .min(4, 'Username should be at least 4 chars'),
+
+    email: yup.string('E-mail should be string')
+        .required('E-mail is required')
+        .email(),
+
+    password: yup.string('Password should be string')
+        .required('Password is required')
+        .min(6, 'Password should be at least 6 chars'),
+
+    rePassword: yup.string('Password should be string')
+        .oneOf([yup.ref('password'), null], 'Passwords don\'t match')
+        .required('Password is required')
+        .min(6, 'Password should be at least 6 chars')
+
+})
+
+export default withForm(Register, initialFormState, schema)
--- src/hocs/formManager.js
+++ src/hocs/formManager.js
@@ -1,20 +1,71 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default function withForm(Cmp, initialState, schema){
-    return class extends Component{
+export default function withForm(Cmp, initialState, schema) {
+    return class extends React.Component {
         state = {
             form: initialState,
-            error: null
+            errors: undefined
         }
 
-        controlChangeHandlerFactory = name => e => {
-            this.setState(({ form }) => {
-                return { form: { ...form, [name]: newValue }}
-            })
+        controlChangeHandlerFactory = name => {
+            let id;
+            return e => {
+                const newValue = e.target.value;
+                if (id) {
+                    clearTimeout(id)
+                    id = null
+                }
+                id = setTimeout(() => {
+                    this.setState(({ form }) => {
+                        return { form: { ...form, [name]: newValue } }
+                    });
+
+                    this.runControlValidation(name)
+                    .then(() => {
+                        this.setState(({ errors: { [name]: current, ...others} = {} }) => ({ errors: others }));
+                    })
+                    .catch(err => {
+                        this.setState(({ errors }) => ({ errors: {...errors, [name]: err.errors }}));
+                    })
+                    id = null;
+                }, 300)
+            }
+        }
+
+        getFormState = () => {
+            return this.state.form;
+        }
+
+        getFormErrorState = () => {
+            return this.state.errors
+        }
+
+        runControlValidation = name => {
+            const currentValue = this.state.form[name]
+            return schema.fields[name].validate(currentValue, { abortEarly: false })
+        }
+
+        runValidations = () => {
+            return schema.validate(this.state.form, { abortEarly: false })
+                .then(() => {
+                    this.setState({errors: undefined})
+                    return this.state.form;
+                }).catch(err => {
+                    const errors = err.inner.reduce((acc, { path, message }) => {
+                        acc[path] = (acc[path] || []).concat(message);
+                        return acc;
+                    }, {});
+                    this.setState({ errors });
+                });
         }
 
         render() {
-            return <Cmp {...this.props} controlChangeHandlerFactory={this.controlChangeHandlerFactory}></Cmp>
+            return <Cmp {...this.props}
+                controlChangeHandlerFactory={this.controlChangeHandlerFactory}
+                getFormState={this.getFormState}
+                runValidations={this.runValidations}
+                getFormErrorState={this.getFormErrorState}
+            ></Cmp>
         }
     }
 }
